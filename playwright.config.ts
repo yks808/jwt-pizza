dff4f0f8414f75088065122319369cdd0789b379
yks808 <yukina.k.soga@gmail.com>
2025-02-12 09:35:47 -0700

    update timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,6 +26,6 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
-    timeout: 5000,
+    timeout: 10000,
   },
 });
