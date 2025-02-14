0eec14e90bdaffb7324bfb0274ca90cd57ad6e52
yks808 <yukina.k.soga@gmail.com>
2025-02-14 10:09:18 -0700

    Delete one of the test function

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -772,15 +772,3 @@ test('franchise create and delete as admin', async ({ page }) => {
   await expect(page.getByRole('list')).toContainText('close-franchise');
   await page.getByRole('button', { name: 'Close' }).click();
 });
-
-test('abc', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  await expect(page.getByLabel('Global').locator('span')).toContainText('JWT Pizza');
-  await expect(page.getByRole('heading')).toContainText('The web\'s best pizza');
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Logout' }).click();
-});
